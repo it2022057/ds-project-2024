0274bbbeec27363c8c9ce78c99283002290b64fa
it2022057 <it2022057@hua.gr>
2024-12-13 17:07:47 +0200

    Initial Commit

---
+++ src/main/java/gr/hua/dit/ds/ds_project_2024/DsProject2024Application.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.ds.ds_project_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsProject2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsProject2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/gr/hua/dit/ds/ds_project_2024/DsProject2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.ds.ds_project_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsProject2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
