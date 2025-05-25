23e89d21da600443d9853f1c9d4161dfe029854e
it2022057 <it2022057@hua.gr>
2025-05-25 23:32:46 +0300

    Used mailhog for SMTP e-mails

--- src/main/java/gr/hua/dit/ds/ds_project_2024/controllers/AuthController.java
+++ src/main/java/gr/hua/dit/ds/ds_project_2024/controllers/AuthController.java
@@ -1,9 +1,7 @@
 package gr.hua.dit.ds.ds_project_2024.controllers;
 
 import gr.hua.dit.ds.ds_project_2024.entities.Role;
-import gr.hua.dit.ds.ds_project_2024.entities.User;
 import gr.hua.dit.ds.ds_project_2024.repositories.RoleRepository;
-import gr.hua.dit.ds.ds_project_2024.service.UserService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/gr/hua/dit/ds/ds_project_2024/service/MailService.java
@@ -0,0 +1,28 @@
+package gr.hua.dit.ds.ds_project_2024.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public MailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendMail(String to, String from, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        mailSender.send(message);
+    }
+}
+
