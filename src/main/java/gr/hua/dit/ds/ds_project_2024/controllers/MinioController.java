626eb1ac7c95620baabf0756823c013d1521189d
it2022057 <it2022057@hua.gr>
2025-05-30 16:12:00 +0300

    MinIO now runs correctly

--- src/main/java/gr/hua/dit/ds/ds_project_2024/controllers/MinioController.java
+++ src/main/java/gr/hua/dit/ds/ds_project_2024/controllers/MinioController.java
@@ -3,11 +3,12 @@ package gr.hua.dit.ds.ds_project_2024.controllers;
 import gr.hua.dit.ds.ds_project_2024.service.MinioService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,21 +19,30 @@ public class MinioController {
     @Autowired
     private MinioService minioService;
 
-    @Value("${minio.bucket.name}")
-    private String bucketName;
-
     @PostConstruct
     public ResponseEntity<String> run() {
-        String[] filenames = {"dog.jpg", "dog2.jpg", "dog3.jpg", "dog4.jpg"};
+        String[] filenames = {"dog1.jpg", "dog2.jpg", "dog3.jpg", "dog4.jpg"};
 
         for (String filename : filenames) {
             try (InputStream is = new ClassPathResource("static/images/" + filename).getInputStream()) {
-                minioService.uploadFile(bucketName, filename, is, "image/jpeg");
+                minioService.uploadFile(filename, is, "image/jpeg");
             } catch (IOException e) {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                         .body("Failed to upload " + filename + ": " + e.getMessage());
             }
         }
         return ResponseEntity.ok("All images uploaded successfully.");
+    }
+
+    @RequestMapping("/upload")
+    public void uploadFile(MultipartFile file) {
+        try {
+            minioService.uploadFile(
+                    file.getOriginalFilename(),
+                    file.getInputStream(),
+                    file.getContentType());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to upload " + file.getOriginalFilename() + ": " + e.getMessage());
+        }
     }
 }
--- src/main/java/gr/hua/dit/ds/ds_project_2024/service/MinioService.java
+++ src/main/java/gr/hua/dit/ds/ds_project_2024/service/MinioService.java
@@ -1,32 +1,65 @@
 package gr.hua.dit.ds.ds_project_2024.service;
 
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import io.minio.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class MinioService {
+
     @Autowired
     private MinioClient minioClient;
 
-    public void uploadFile(String bucketName, String objectName, InputStream inputStream, String contentType) {
+    @Value("${minio.bucket.name}")
+    private String bucketName;
+
+    public void uploadFile(String objectName, InputStream inputStream, String contentType) {
         try {
             boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
             if (!found) {
                 minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
             }
-            minioClient.putObject(
-                    PutObjectArgs.builder().bucket(bucketName).object(objectName).stream(
-                                    inputStream, inputStream.available(), -1)
+            minioClient.putObject(PutObjectArgs
+                            .builder()
+                            .bucket(bucketName)
+                            .object(objectName)
+                            .stream(inputStream, inputStream.available(), -1)
                             .contentType(contentType)
                             .build());
         } catch (Exception e) {
             throw new RuntimeException("Error occurred: " + e.getMessage());
+        }
+    }
+
+    public void downloadFile(String objectName, String destinationPath) {
+        try (InputStream stream = minioClient.getObject(
+                GetObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(objectName)
+                        .build())) {
+
+            Files.copy(stream, Paths.get(destinationPath), StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            throw new RuntimeException("Error downloading file: " + e.getMessage(), e);
+        }
+    }
+
+    public byte[] getFile(String objectName) {
+        try (InputStream stream =
+                     minioClient.getObject(GetObjectArgs
+                             .builder()
+                             .bucket(bucketName)
+                             .object(objectName)
+                             .build())) {
+            return stream.readAllBytes();
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving file from MinIO: " + e.getMessage());
         }
     }
 }
