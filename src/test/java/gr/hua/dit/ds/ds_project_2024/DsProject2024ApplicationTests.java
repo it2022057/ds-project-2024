ecb3ad6e76e46b385f4761ed79e242e2d22fc1dc
it2022057 <it2022057@hua.gr>
2025-06-06 16:32:27 +0300

    Added some more unit test files

--- src/test/java/gr/hua/dit/ds/ds_project_2024/DsProject2024ApplicationTests.java
+++ src/test/java/gr/hua/dit/ds/ds_project_2024/DsProject2024ApplicationTests.java
@@ -2,7 +2,6 @@ package gr.hua.dit.ds.ds_project_2024;
 
 import gr.hua.dit.ds.ds_project_2024.entities.User;
 import gr.hua.dit.ds.ds_project_2024.repositories.UserRepository;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +20,7 @@ class DsLab2024ApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
+
 	@Autowired
 	private UserRepository userRepository;
 
@@ -29,20 +29,78 @@ class DsLab2024ApplicationTests {
 	}
 
 	@Test
-	@Transactional
 	public void testCreateUser() throws Exception {
 		// Arrange
 		String userHtml = "username=apiuser&password=pass123&email=apiuser@hua.gr&phone=6946586841";
 
 		// Act
-		ResultActions result1 = mockMvc.perform(post("/saveUser")
+		ResultActions result = mockMvc.perform(post("/saveUser")
 				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
 				.with(csrf())
 				.content(userHtml));
 
 		// Assert
-		result1.andExpect(status().isOk());
+		result.andExpect(status().isOk());
+	}
 
+	@Test
+	public void testLoginUser() throws Exception {
+		// User credentials
+		String userHtml = "username=loginuser&password=pass12345&email=loginuser@hua.gr&phone=6966896842";
+
+		// Create a user
+		mockMvc.perform(post("/saveUser")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.with(csrf())
+				.content(userHtml))
+				.andExpect(status().isOk());
+
+		// Then proceed with log in
+		ResultActions result = mockMvc.perform(post("/login")
+				.param("username", "loginuser")
+				.param("password", "pass12345")
+				.with(csrf()));
+
+		// Assert
+		result
+				.andExpect(status().is3xxRedirection()) // Typically redirects on successful login
+				.andExpect(redirectedUrl("/")); // at home page
+	}
+
+	@Test
+	public void testInvalidLogin() throws Exception {
+		mockMvc.perform(post("/login")
+						.param("username", "invalid")
+						.param("password", "wrong")
+						.with(csrf()))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/login?error=true"));
+	}
+
+	// Secured page requires login
+	@Test
+	public void testSecuredPageRedirectsToLogin() throws Exception {
+		mockMvc.perform(get("/pet")) // protected page
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrlPattern("**/login"));
+	}
+
+	@Test
+	public void testCreatePet() throws Exception {
+		// Info fields for the new pet
+		String petForm = "name=Rex&age=3&species=Dog&sex=Male&approval_status=0&image_path=http://play.min.io/pet-adoption-app/pet-photos/Rex.jpg";
+
+		// Save new pet
+		mockMvc.perform(post("/pet/new")
+						.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+						.with(csrf())
+						.content(petForm))
+				.andExpect(status().is3xxRedirection());
+	}
+
+	@Test
+	public void testDeleteUser() throws Exception {
+		// Find the user to delete
 		User user = userRepository.findByUsername("apiuser")
 				.orElseThrow(() -> new RuntimeException("User not found in DB"));
 
@@ -52,27 +110,5 @@ class DsLab2024ApplicationTests {
 
 		// Assert
 		result2.andExpect(status().is3xxRedirection());
-	}
-
-	@Test
-	public void testLoginUser() throws Exception {
-		// Arrange
-		String userHtml = "username=apiuser&password=pass123";
-
-		// Act
-		ResultActions result = mockMvc.perform(get("/login")
-				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-				.content(userHtml));
-
-		// Assert
-		result.andExpect(status().isOk());
-	}
-
-	// Secured page requires login
-	@Test
-	public void testSecuredPageRedirectsToLogin() throws Exception {
-		mockMvc.perform(get("/pet")) // protected page
-				.andExpect(status().is3xxRedirection())
-				.andExpect(redirectedUrlPattern("**/login"));
 	}
 }
